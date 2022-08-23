ec352e7f083bac501a3e1cd88cb4c8f051f8c8ee
bp26 <94074742+bp26@users.noreply.github.com>
2022-08-23 07:57:13 +0000

    feat: add some functions

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import blueCards from './data/mythicCards/blue/index.js'
 import brownCards from './data/mythicCards/brown/index.js'
 
 
+
 let current_ancient = 'azathoth'
 let current_difficulty = 'normal'
 
@@ -17,33 +18,51 @@ function getAncient() {
 function countCards() {
     const current_object = getAncient()
     return {
-        'greenCards': current_object.firstStage.greenCards + current_object.secondStage.greenCards + current_object.thirdStage.greenCards,
-        'blueCards': current_object.firstStage.blueCards + current_object.secondStage.blueCards + current_object.thirdStage.blueCards,
-        'brownCards': current_object.firstStage.brownCards + current_object.secondStage.brownCards + current_object.thirdStage.brownCards   
+        'greenCardsCount': current_object.firstStage.greenCards + current_object.secondStage.greenCards + current_object.thirdStage.greenCards,
+        'blueCardsCount': current_object.firstStage.blueCards + current_object.secondStage.blueCards + current_object.thirdStage.blueCards,
+        'brownCardsCount': current_object.firstStage.brownCards + current_object.secondStage.brownCards + current_object.thirdStage.brownCards   
     }
 }
 
-function setArrays() {
-    const greenArr =[]
-    const blueArr = []
-    const brownArr = []
-    
-    /*switch (current_difficulty) {
+function shuffleInitialArrays() { 
+    return {
+        'greenCards': _.shuffle(greenCards),
+        'blueCards': _.shuffle(blueCards),
+        'brownCards': _.shuffle(brownCards)
+    }
+}
+
+function getFinalArrays() {
+    const cardsCount = countCards()
+    const initialArrays = shuffleInitialArrays()
+    switch(current_difficulty) {
         case 'normal':
-            function setNormalArray() {
-
-            }
-    }*/
-
-
+            return getNormalArray(cardsCount, initialArrays)
+    }
 }
 
+function
 
-function setNormalArray() {
-    
+function getNormalArray(cardsCount, initialArrays) {
+    const green = []
+    const blue = []
+    const brown = []
+
+
+    for (let i = 0; i < cardsCount.greenCardsCount; i++) {
+        green.push(initialArrays.greenCards.pop())
+    } 
+    for (let i = 0; i < cardsCount.blueCardsCount; i++) {
+        green.push(initialArrays.blueCards.pop())
+    } 
+    for (let i = 0; i < cardsCount.brownCardsCount; i++) {
+        green.push(initialArrays.greenCards.pop())
+    } 
 }
 
-console.log(greenCards)
+console.log(getFinalArrays())
+
+
 
 
 
